9e026528458e9de0b198fb04620c76af349014f4
imufiid <imufiid98@gmail.com>
2021-09-22 15:19:29 +0700

    create helper file, +helper get env field

---
+++ app/helper/helper.go
@@ -0,0 +1,20 @@
+package helper
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func GetENV(key, fallback string) string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
